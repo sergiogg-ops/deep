1c3cee000544446644f5733d9a4784f97fc49447
sergiogg-ops <sergiogg1@gmail.com>
2025-09-08 13:04:42 +0200

    setup para BEER

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
+# !/bin/bash
+# FastWER calculation
 pip install -r requirements.txt
 git clone https://github.com/PRHLT/fastwer.git
 cd fastwer
 python setup.py install
 cd ..
+
+# BEER installation
+wget https://raw.githubusercontent.com/stanojevic/beer/master/packaged/beer_2.0.tar.gz
+tar xfvz beer_2.0.tar.gz
+rm beer_2.0.tar.gz
